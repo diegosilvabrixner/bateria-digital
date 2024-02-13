4558350d06edaf706c8ba94e0f72598d8628c85d
diegosilvabrixner <125466485+diegosilvabrixner@users.noreply.github.com>
2024-02-13 17:31:49 -0300

    Add files via upload
    projeto que simula uma bateria, com 9 sons variados, o projeto teve foco o desenvolvimento do Javascript e foi criado com o objetivo de estudos com o apoio de um instrutor da Alura.

---
+++ main.js
@@ -0,0 +1,39 @@
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if(elemento && elemento.localName === 'audio'){
+            elemento.play();
+    }
+    else{
+        //quando for publicar, troca a mensagem(usar do console.log)
+         alert('isso ai não existe não');
+        //console.log('elemento ou seletor não encontrado');
+    }
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++ ) {
+
+    const tecla= listaDeTeclas[contador]
+    const instrumento = tecla.classList[1];
+    const idAudio =`#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('ativa');
+        }
+
+    }
+
+    tecla.onkeyup = function (){
+        tecla.classList.remove('ativa'); 
+    }
+
+}
